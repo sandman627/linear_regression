75ab8716749cdcee1e6e1bf0dd7920f89062dcc8
hart <hart627@g.skku.edu>
2021-08-02 21:54:10 +0900

    first commit

---
+++ linearRegression.py
@@ -0,0 +1,22 @@
+
+
+
+
+class LinearRegression:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.list_parameter = []
+
+
+    def hypothesis(self):
+
+
+
+
+    def cost_function(self):
+
+
+
+
+
+    def gradient_descent(self):
